99ced18932b715cd684b5c90f0bd1d93412b9875
braj011 <bhagyaraj01@gmail.com>
2018-08-20 16:14:12 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,6 @@
 def oxford_comma(array)
   if array.size  == 1
     array.join
-  else if array.size  == 2
-    last2 = array.pop
-    x2  = array.join
-    final = "#{x2} and #{last2}" 
   else 
     last = array.pop
     x  = array.join(", ") 
